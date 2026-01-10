f20a9d6b284478a99376febb582e0e6fcc8feec0
Ankit Gupta <ankit.gupta.ext@sivantos.com>
2026-01-10 22:35:19 +0530

    added Animation

--- SwiftUI_UIReplication/Shared/Product.swift
+++ SwiftUI_UIReplication/Shared/Product.swift
@@ -6,16 +6,20 @@ struct ProductArray: Codable {
 }
 
 struct Product: Codable, Identifiable {
-  let id: Int
-  let title, description: String
-  let price: Double
-  let discountPercentage, rating: Double
-  let stock: Int
-  let brand: String?
-  let category: String
-  let thumbnail: String
-  let images: [String]
-  let meta: ProductMetaData
+    let id: Int
+    let title, description: String
+    let price: Double
+    let discountPercentage, rating: Double
+    let stock: Int
+    let brand: String?
+    let category: String
+    let thumbnail: String
+    let images: [String]
+    let meta: ProductMetaData
+    
+    let isRecent: Bool = {
+        Int.random(in: 1...4) == 1
+    }()
 }
 
 struct ProductMetaData: Codable {
